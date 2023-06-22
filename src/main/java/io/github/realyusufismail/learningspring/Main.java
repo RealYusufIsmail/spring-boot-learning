fa06e5716c883b43a4a1398c0b96289e4ad1b7e1
RealYusufIsmail <yusufgamer222@gmail.com>
2023-06-22 21:36:46 +0100

    Added ability to GET data

--- src/main/java/io/github/realyusufismail/learningspring/Main.java
+++ src/main/java/io/github/realyusufismail/learningspring/Main.java
@@ -3,38 +3,47 @@ package io.github.realyusufismail.learningspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//Needed to indicate that this is a Spring Boot application
-@SpringBootApplication // needed for the application to run
-//Makes this class a controller that can handle HTTP requests
+@SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
 
-    /**
-     * The main method of the application
-     *
-     * @param args anything that comes from the command line
-     */
+    private final CustomerRepo customerRepo;
+
+    public Main(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
     public static void main(String[] args) {
-        //Needed to run the Spring Boot application
         SpringApplication.run(Main.class, args);
-
-        //Tomcat is a web server that is used to run Java web applications
     }
 
-    //In order to expose method as a rest endpoint, we need to annotate it with @GetMapping
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        // if record is just returned like this, it will just give you the toString() of the record but if @RestController is used, it will automatically convert it to JSON
-        return new GreetResponse("Hello World!", List.of(new favProgrammingLanguage("Java"), new favProgrammingLanguage("Kotlin")));
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepo.findAll();
     }
 
-    public record favProgrammingLanguage(String language) { }
 
-    // records are object that are immutable and have a constructor that takes all the fields
-    //immutable means that once the object is created, it cannot be changed
-    public record GreetResponse(String message, List<favProgrammingLanguage> languages) { }
+    /**
+     * nextval automatically increments the id
+     * @return the terminal code that is used to insert data into the database
+     */
+    private String terminalCode() {
+        return """
+                customer=# INSERT INTO customer(id, name, email, age)\s
+                customer-# VALUES (nextval('customer_id_sequence'), 'yusuf', 'yusuf@gamil.com', 16)
+                customer-# ;
+                INSERT 0 1
+                customer=# INSERT INTO customer(id, name, email, age)\s
+                VALUES (nextval('customer_id_sequence'), 'bob', 'bob@gmail.com', 21);
+                INSERT 0 1
+                customer=#\s
+                                
+                """;
+    }
 }
---
+++ src/main/java/io/github/realyusufismail/learningspring/pastlessons/OldMain.java
@@ -0,0 +1,38 @@
+package io.github.realyusufismail.learningspring.pastlessons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+//Needed to indicate that this is a Spring Boot application
+//@SpringBootApplication // needed for the application to run
+//Makes this class a controller that can handle HTTP requests
+//@RestController
+public class OldMain {
+
+    /**
+     * The main method of the application
+     *
+     * @param args anything that comes from the command line
+     */
+    public static void main(String[] args) {
+        //Needed to run the Spring Boot application
+        SpringApplication.run(OldMain.class, args);
+
+        //Tomcat is a web server that is used to run Java web applications
+    }
+
+    //In order to expose method as a rest endpoint, we need to annotate it with @GetMapping
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        // if record is just returned like this, it will just give you the toString() of the record but if @RestController is used, it will automatically convert it to JSON
+        return new GreetResponse("Hello World!", List.of(new favProgrammingLanguage("Java"), new favProgrammingLanguage("Kotlin")));
+    }
+
+    public record favProgrammingLanguage(String language) { }
+
+    // records are object that are immutable and have a constructor that takes all the fields
+    //immutable means that once the object is created, it cannot be changed
+    public record GreetResponse(String message, List<favProgrammingLanguage> languages) { }
+}
