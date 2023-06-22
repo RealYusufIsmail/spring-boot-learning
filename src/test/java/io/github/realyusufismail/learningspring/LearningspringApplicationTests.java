a9c4842a444a2b1bb5b1a619daf353407decdb45
RealYusufIsmail <yusufgamer222@gmail.com>
2023-06-22 14:00:54 +0100

    Initial commit

---
+++ src/test/java/io/github/realyusufismail/learningspring/LearningspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.realyusufismail.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
